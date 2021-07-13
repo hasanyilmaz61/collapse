38a769744864931182bd89f1f06d2242c4be9856
hasanyilmaz61 <83864802+hasanyilmaz61@users.noreply.github.com>
2021-07-13 20:11:54 +0200

    html akustilieren

--- ornek.js
+++ ornek.js
@@ -1,19 +1,24 @@
-const conteiner=document.getElementById("conteiner");
-
+let conteiner=document.getElementById("conteiner");
+let icon = document.getElementById("sembol");
 let auf=true;
 function klikt() {
 
-if(auf){
+    if (auf) {
+        conteiner.style.height="180px";
+        icon.style.transform="rotate(180deg)"
 
-conteiner.style.height="100px";
-auf=false;
-}
+        auf=false;
+    }
+    else{
+        
 
-else{
+        conteiner.style.height="100px";
+        icon.style.transform="rotate(0deg)"
 
-    conteiner.style.height="180px";
-    auf=true;
-}
+         auf=true;
+    }
+
 
     
 }
+
